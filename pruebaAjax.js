68f4d5733e842dd6bbdc5972cff806d3b2114f5e
Freddie8606 <ing_julianmadrid@hotmail.com>
2019-11-25 14:30:32 -0500

    Primeros cambios en Ajax

--- pruebaAjax.js
+++ pruebaAjax.js
@@ -1,1 +1,11 @@
 //AJAX = Asynchronous Javascript and XML
+
+const listarPersonajes = async () =>
+{
+    const response = await fetch("https://swapi.co/api/people/");
+    const myJson = await response.json();
+    //console.log(JSON.stringify(myJson));
+    console.log(myJson);
+};
+
+listarPersonajes();
