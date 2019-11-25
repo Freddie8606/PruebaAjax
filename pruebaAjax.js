04209584c1c385998f2a942dccd7f79d7e87b8b5
Freddie8606 <ing_julianmadrid@hotmail.com>
2019-11-25 14:43:31 -0500

    Cambio de título e inserción de tabla

--- pruebaAjax.js
+++ pruebaAjax.js
@@ -1,11 +1,30 @@
 //AJAX = Asynchronous Javascript and XML
 
+function crearTablaParaListar(personajes)
+{
+    if(personajes && personajes.lenght)
+    {
+        let htmlFinal = "";
+        htmlFinal = `<table>
+                        <thead>
+                            <tr>
+                                <th>Nombre</th>
+                                <th>Color Pelo</th>
+                                <th>Color Ojos</th>
+                            </tr>
+                        </thead>`; 
+        htmlFinal += `</table>`
+    }
+}
+
 const listarPersonajes = async () =>
 {
     const response = await fetch("https://swapi.co/api/people/");
     const myJson = await response.json();
     //console.log(JSON.stringify(myJson));
     console.log(myJson);
+    const personajes = myJson.results;
+    console.log(personajes);
 };
 
 listarPersonajes();
